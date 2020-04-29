7433bf9b29915d63368287ba8a15db8a41585e30
euroakung <nopparat.p259@gmail.com>
2020-04-30 03:43:20 +0700

    commit

--- src/app/components/bootstrap/bootstrap.module.ts
+++ src/app/components/bootstrap/bootstrap.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
-import {
-  AccordionModule } from 'ngx-bootstrap/accordion';
-import {  AlertModule  } from 'ngx-bootstrap/alert';
-import {  CollapseModule  } from 'ngx-bootstrap/collapse';
-import {  BsDropdownModule  } from 'ngx-bootstrap/dropdown';
-import {  ModalModule  } from 'ngx-bootstrap/modal';
-import {  PaginationModule  } from 'ngx-bootstrap/pagination';
-import {  PopoverModule  } from 'ngx-bootstrap/popover';
-import {  ProgressbarModule  } from 'ngx-bootstrap/progressbar';
-import {  SortableModule  } from 'ngx-bootstrap/sortable';
-import {  TabsModule  } from 'ngx-bootstrap/tabs';
-import {  TooltipModule  } from 'ngx-bootstrap/tooltip';
-import {  TypeaheadModule  } from 'ngx-bootstrap/typeahead';
-import {  ButtonsModule  } from 'ngx-bootstrap/buttons';
-
-
+import {  AccordionModule } from 'ngx-bootstrap/accordion';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PopoverModule } from 'ngx-bootstrap/popover';
+import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
+import { SortableModule } from 'ngx-bootstrap/sortable';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { RatingModule } from 'ngx-bootstrap/rating';
 @NgModule({
   declarations: [],
   imports: [
@@ -30,7 +28,8 @@ import {  ButtonsModule  } from 'ngx-bootstrap/buttons';
     SortableModule.forRoot(),
     TabsModule.forRoot(),
     TooltipModule.forRoot(),
-    TypeaheadModule.forRoot()
+    TypeaheadModule.forRoot(),
+    RatingModule.forRoot()
   ],
   exports: [
     AccordionModule,
@@ -45,7 +44,8 @@ import {  ButtonsModule  } from 'ngx-bootstrap/buttons';
     SortableModule,
     TabsModule,
     TooltipModule,
-    TypeaheadModule
+    TypeaheadModule,
+    RatingModule
   ]
 })
 export class BootstrapModule { }
--- src/app/containers/forms/datepicker/datepicker.component.ts
+++ src/app/containers/forms/datepicker/datepicker.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { BsLocaleService } from 'ngx-bootstrap/datepicker';
-import { defineLocale } from 'ngx-bootstrap/chronos'; 
-import { nbLocale } from "ngx-bootstrap/locale";
-defineLocale("th", nbLocale);
+import { BsLocaleService } from 'ngx-bootstrap/datepicker'; 
 @Component({
   selector: 'app-datepicker',
   templateUrl: './datepicker.component.html'
@@ -13,21 +10,19 @@ export class DatepickerComponent implements OnInit {
   form: FormGroup;
   bsValue = new Date();
   bsRangeValue: Date[];
-  maxDate = new Date();
-
+  maxDate = new Date(); 
 
   bsInlineValue = new Date();
 
 
-  constructor(private localeService: BsLocaleService) {
-    this.localeService.use('th');
+  constructor(private localeService: BsLocaleService) { 
     this.maxDate.setDate(this.maxDate.getDate() + 7);
     this.bsRangeValue = [this.bsValue, this.maxDate];
     
   
   }
 
-  ngOnInit() {
+  ngOnInit() { 
     this.form = new FormGroup({
       basicDate: new FormControl(new Date()),
     });
